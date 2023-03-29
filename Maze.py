7651aa5efb539ed59e0c68caf66a069a3f40e818
21037788 <21037788@learning.curtin.edu.sg>
2023-03-28 23:30:14 -0700

    change currentRow currentCol to row col

--- Maze.py
+++ Maze.py
@@ -1,4 +1,4 @@
-import GridViewer
+import Viewer
 
 EMPTY = 0
 WALL = 1
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         [ WALL,  EMPTY,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
     ]
                     
-    GridViewer.view(grid)
+    Viewer.view(grid)
 
 
     print("Find a solution to get from ^^ to $$, using the characters " 
@@ -31,8 +31,8 @@ if __name__ == "__main__":
         + " (for north, east, south and west).")
     solution = input("Your solution: ")
 
-    currentRow = 1
-    currentCol = 0
+    row = 1
+    col = 0
     done = False
     solved = False
     charIndex = 0
@@ -41,39 +41,39 @@ if __name__ == "__main__":
     while not done and charIndex < solutionLength:
         
         direction = solution[charIndex]
-        print("Location: (" + str(currentRow) + ", " + str(currentCol) 
+        print("Location: (" + str(row) + ", " + str(col) 
             + "), next direction: '" + direction + "'")
         
         if direction == NORTH:
-            currentRow -= 1
+            row -= 1
             
         elif direction == EAST:
-            currentCol += 1
+            col += 1
                 
         elif direction == SOUTH:
-            currentRow += 1
+            row += 1
                 
         elif direction == WEST:
-            currentCol -= 1
+            col -= 1
         
         else:
             print("You are no idea where you're going") # Invalid direction.
         
-        if (currentRow < 0 or currentCol < 0 
-                        or currentRow >= len(grid) 
-                        or currentCol >= len(grid[currentRow])):
+        if (row < 0 or col < 0 
+                        or row >= len(grid) 
+                        or col >= len(grid[row])):
             done = True
             print("You faik into the chasm of done") # Out of bounds.
             
         else:
-            if grid[currentRow][currentCol] == EMPTY:
-                grid[currentRow][currentCol] = VISITED
+            if grid[row][col] == EMPTY:
+                grid[row][col] = VISITED
                 
-            elif grid[currentRow][currentCol] == WALL:
+            elif grid[row][col] == WALL:
                 done = True
                 print("You stumble blindly into a solid concrete wall") # Hit wall.
 
-            elif grid[currentRow][currentCol] == END:
+            elif grid[row][col] == END:
                 done = True
                 solved = True
                 print("SOLVED!") # Solved.
@@ -88,4 +88,4 @@ if __name__ == "__main__":
     if not solved:
         print("You have failed to escape. Future archeologists gaze upon your remains in bafflement") # Did not reach the end.
 
-    GridViewer.view(grid)
+    Viewer.view(grid)
